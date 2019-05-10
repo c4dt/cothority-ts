83ce1c89bed8e67a9c1a90bc894076112d73d1a9
tharvik <tharvik@users.noreply.github.com>
2019-05-10 15:34:24 +0200

    use interfaces

--- external/js/cothority/src/network/protobuf.ts
+++ external/js/cothority/src/network/protobuf.ts
@@ -1,6 +1,13 @@
 import { IConnection } from "./connections";
 
-export default async function rpc<U,V>(conn: IConnection, msg: { encode: U => Uint8Array }, decoder: { decode: Uint8Array => V }: Promise<V> {
+interface Encoder<T> {
+	encode(_: T): Uint8Array;
+}
+interface Decoder<T> {
+	decode(_: Uint8Array): T;
+}
+
+export default async function rpc<U,V>(conn: IConnection, msg: Encoder<U>, decoder: Decoder<V>): Promise<V> {
     await conn.sendmsg(msg.encode(msg).finish());
     return decoder(await conn.recvmsg());
 }
